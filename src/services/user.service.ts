5df7213a79784f714d20143e3dfc1471013e5a07
Bruno Tenreiro <b.tenreiro@xgeeks.com>
2026-04-10 09:41:38 +0100

    chore: fix pipeline

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import type { User } from '@prisma/client'
+import type { User } from '@/generated/prisma/client/client'
 import { NotFoundError } from '@/errors/not-found.error'
 import type { UserRepository } from '@/repositories/user.repository'
 
